5f01a0b3c666e6c0764e088bccbbe2c660e6be26
Oliver Toth <90266732+TothOliver@users.noreply.github.com>
2025-11-17 14:28:29 +0100

    some setup here  for main

--- Assignment2/Memory.hpp
+++ Assignment2/Memory.hpp
@@ -2,5 +2,5 @@
 
 #include <cstddef>
 
-void* CustomAlloc(size_t size);
+void* CustomAlloc(size_t size, size_t objectCount, bool custom);
 void  CustomFree(void* ptr);
--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
 #include <cstdint>
 #include <windows.h>
+#include "Timer.hpp"
 #include "Memory.hpp"
 #include "PoolAllocator.hpp"
+
 
 struct Object{
 	std::uint8_t data[32];
 };
 
+enum class AllocMode{
+	OS,
+	Pool
+}
+
+double runTestAllocation(AllocMode mode, int objectCount){
+ 	std::cout << "Running test with " 
+		<< (mode == AllocMode::OS ? "OS allocator" : "Pool allocator") 
+		<< " (" << objectCount << " objects)...\n";
+
+	static PoolAllocator pool(sizeof(Object), 100000);
+
+	std::vector<void*> pointers;
+	pointers.reverse(objectCount);
+
+	Timer timer;
+	timer.start();
+
+	if(mode == AllocMode::OS){
+		for(int i = 0; i < objectCount; i++)
+	}
+	else if(mode == AllocMode.Pool){
+		
+	}
+}
+
 int main()
 {
-	//PoolAllocator pool(sizeof(Object), 100);
-	CustomAlloc(sizeof(Object));
-	while (true)
-	{
+	const int objectCount = 10000;
 
-		std::cout << "LOOPIN" << std::endl;
-		if (GetKeyState(VK_ESCAPE) & 0x8000)
-		{
-			break;
-		}
-	}
-	return 0;
+	double osTime = runTestAllocation(AllocMode::OS, objectCount);
+	//double poolTime = runTestAllocation(AllocMode::Pool, object);
+
+    std::cout << "Summary:\n";
+    std::cout << "  OS allocator time:   " << osTime   << " ms\n";
+    std::cout << "  Pool allocator time: " << poolTime << " ms\n";
+
+    return 0;
 }
-//herro
