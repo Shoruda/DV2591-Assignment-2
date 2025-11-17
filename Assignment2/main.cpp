984c9b007a000681ef368843f8b637cfb9864335
Shoruda <axel.bergaman@gmail.com>
2025-11-17 11:51:35 +0100

    loopin

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
 	std::cout << "Hello World!" << std::endl;
+	while (true)
+	{
+		std::cout << "LOOPIN" << std::endl;
+		if (GetKeyState(VK_ESCAPE) & 0x8000)
+		{
+			break;
+		}
+	}
 	return 0;
 }
 //herro
