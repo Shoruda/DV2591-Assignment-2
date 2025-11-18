8f23f6360642f8ea87c199c62a4d37d993580c49
Oliver Toth <90266732+TothOliver@users.noreply.github.com>
2025-11-18 12:23:16 +0100

    allocate done? (i can program in c++ yes)

--- Assignment2/StackAllocator.cpp
+++ Assignment2/StackAllocator.cpp
@@ -16,6 +16,25 @@ StackAllocator::~StackAllocator()
     m_offset = 0;
 }
 
-void* Allocate(size_t size, size_t alignment){
+static size_t AlignUp(size_t value, size_t alignment)
+{
+    size_t rest = value % alignment;
+    if(rest == 0)
+        return 0;
 
+    return value + (alignment - rest);
+}
+
+void* StackAllocator::Allocate(size_t size, size_t alignment)
+{
+    size_t alignedOffset = AlignUp(m_offset, alignment);
+    if(alignedOffset + size > m_capacity){
+        std::cout << "Error: Stack Allocator not enough capacity left" << std::endl;
+        return nullptr;
+    }
+
+    void* ptr = m_base + alignedOffset;
+    m_offset = alignedOffset + size;
+
+    return ptr;
 }
