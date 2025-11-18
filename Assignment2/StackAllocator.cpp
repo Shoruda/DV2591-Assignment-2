ae099f64ab99926106028099c6b44e63a27926a0
Oliver Toth <90266732+TothOliver@users.noreply.github.com>
2025-11-18 12:07:21 +0100

    constroctor/deconstructor

--- Assignment2/StackAllocator.cpp
+++ Assignment2/StackAllocator.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "StackAllocator.hpp"
 
-StackAllocator::StackAllocator(size_t capacity) 
-    :capacity(capacity){
+StackAllocator::StackAllocator(size_t capacity)
+    :m_capacity(capacity)
+{
+    m_base = static_cast<std::uint8_t*>(std::malloc(m_capacity));
+    m_offset = 0;
+}
+
+StackAllocator::~StackAllocator()
+{
+    std::free(m_base);
+    m_base = nullptr;
+    m_capacity = 0;
+    m_offset = 0;
+}
+
+void* Allocate(size_t size, size_t alignment){
 
 }
