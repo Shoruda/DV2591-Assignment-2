e1c068469c06ba1badd7e13df0dcfc179527c81c
Ponlin02 <ponlin.02@gmail.com>
2025-11-18 14:39:39 +0100

    Buddy allocator constructor

--- Assignment2/BuddyAllocator.cpp
+++ Assignment2/BuddyAllocator.cpp
@@ -1,5 +1,21 @@
 #include "BuddyAllocator.hpp"
 
+BuddyAllocator::BuddyAllocator(size_t minBlockSize, size_t totalSize)
+	:m_minBlockSize(minBlockSize), m_totalSize(totalSize)
+{
+	m_basePtr = malloc(totalSize);
+}
+
 BuddyAllocator::~BuddyAllocator()
+{
+	free(m_basePtr);
+}
+
+void* BuddyAllocator::allocate(size_t size)
+{
+	return nullptr;
+}
+
+void BuddyAllocator::deallocate(void* ptr)
 {
 }
