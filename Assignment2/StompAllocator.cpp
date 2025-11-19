2c538b13611fb5f4c056668283983a4439a117c8
GuptaWaltersson <huwa22@student.bth.se>
2025-11-19 11:16:14 +0100

    Update StompAllocator.cpp

--- Assignment2/StompAllocator.cpp
+++ Assignment2/StompAllocator.cpp
@@ -1,5 +1,18 @@
 #include "StompAllocator.hpp"
 
+StompAllocator::StompAllocator()
+{
+}
+
 StompAllocator::~StompAllocator()
+{
+}
+
+void* StompAllocator::allocate(size_t size, size_t alignment)
+{
+	return nullptr;
+}
+
+void StompAllocator::deallocate(void* ptr)
 {
 }
