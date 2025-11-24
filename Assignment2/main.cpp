fed7f3ec11305335b7d2c304c93fe95a0ab89e63
Ponlin02 <ponlin.02@gmail.com>
2025-11-24 12:00:43 +0100

    Made tests

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -8,7 +8,7 @@
 
 
 struct ObjectSmall{
-	std::uint8_t data[32];
+	std::uint8_t data[64];
 };
 struct ObjectMedium{
 	std::uint8_t data[128];
@@ -87,7 +87,7 @@ double runTestAllocation(AllocMode mode, int objectCount){
 
 	if (mode == AllocMode::OS) 
 	{
-		std::cout << "Running test with OS allocator\n";
+		//std::cout << "Running test with OS allocator\n";
 		for (int i = 0; i < objectCount; i += 10)
 		{
 			void* ptrs[10];
@@ -110,7 +110,7 @@ double runTestAllocation(AllocMode mode, int objectCount){
 	
 	else if (mode == AllocMode::Pool) 
 	{
-		std::cout << "Running test with Pool allocator\n";
+		//std::cout << "Running test with Pool allocator\n";
 		InitPool(sizeof(T), 20, 16);
 		for (int i = 0; i < objectCount; i += 10)
 		{
@@ -134,7 +134,7 @@ double runTestAllocation(AllocMode mode, int objectCount){
 	
 	else if (mode == AllocMode::Stack) 
 	{
-		std::cout << "Running test with Stack allocator\n";
+		//std::cout << "Running test with Stack allocator\n";
 		const int frames = 1;
 		const int allocsPerFrame = objectCount / frames;
 		const size_t capacity = sizeof(T) * allocsPerFrame;
@@ -158,7 +158,7 @@ double runTestAllocation(AllocMode mode, int objectCount){
 
 	else if (mode == AllocMode::Buddy)
 	{
-		std::cout << "Running test with Buddy allocator\n";
+		//std::cout << "Running test with Buddy allocator\n";
 		InitBuddy(sizeof(T), sizeof(T) * 16);
 
 		for (int i = 0; i < objectCount; i += 10)
@@ -183,7 +183,7 @@ double runTestAllocation(AllocMode mode, int objectCount){
 
 	else if (mode == AllocMode::Stomp)
 	{
-		std::cout << "Running test with Stomp allocator\n";
+		//std::cout << "Running test with Stomp allocator\n";
 		InitStomp();
 		for (int i = 0; i < objectCount; i+=10)
 		{
@@ -208,26 +208,46 @@ double runTestAllocation(AllocMode mode, int objectCount){
 	
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Test finished in " << duration.count() << " ms\n\n";
+    //std::cout << "Test finished in " << duration.count() << " ms\n\n";
     return duration.count();
+}
+
+template<typename T>
+void runAllTestsForType(const std::string& name, int objectCount)
+{
+	double osTime = 0.0;
+	double poolTime = 0.0;
+	double stackTime = 0.0;
+	double buddyTime = 0.0;
+	double stompTime = 0.0;
+	int iterations = 10;
+
+	for (int i = 0; i < iterations; i++)
+	{
+		osTime += runTestAllocation<T>(AllocMode::OS, objectCount);
+		poolTime += runTestAllocation<T>(AllocMode::Pool, objectCount);
+		stackTime += runTestAllocation<T>(AllocMode::Stack, objectCount);
+		buddyTime += runTestAllocation<T>(AllocMode::Buddy, objectCount);
+		stompTime += runTestAllocation<T>(AllocMode::Stomp, objectCount);
+		ShutdownMemory();
+	}
+
+	std::cout << "\n=== Results for " << name << " ===\n";
+	std::cout << "  OS allocator time:    " << osTime / iterations << " ms\n";
+	std::cout << "  Pool allocator time:  " << poolTime / iterations << " ms\n";
+	std::cout << "  Stack allocator time: " << stackTime / iterations << " ms\n";
+	std::cout << "  Buddy allocator time: " << buddyTime / iterations << " ms\n";
+	std::cout << "  Stomp allocator time: " << stompTime / iterations << " ms\n";
 }
 
 int main()
 {
-	const int objectCount = 10000;
+	const int objectCount = 500000;
 
-	double osTime = runTestAllocation<ObjectSmall>(AllocMode::OS, objectCount);
-	double poolTime = runTestAllocation<ObjectSmall>(AllocMode::Pool, objectCount);
-	double stackTime = runTestAllocation<ObjectSmall>(AllocMode::Stack, objectCount);
-	double buddyTime = runTestAllocation<ObjectSmall>(AllocMode::Buddy, objectCount);
-	double stompTime = runTestAllocation<ObjectSmall>(AllocMode::Stomp, objectCount);
-
-    std::cout << "Summary:\n";
-    std::cout << "  OS allocator time:   " << osTime   << " ms\n";
-    std::cout << "  Pool allocator time: " << poolTime << " ms\n";
-	std::cout << "  Stack allocator time: " << stackTime << " ms\n";
-	std::cout << "  Buddy allocator time: " << buddyTime << " ms\n";
-	std::cout << "  Stomp allocator time: " << stompTime << " ms\n";
+	runAllTestsForType<ObjectSmall>("ObjectSmall", objectCount);
+	runAllTestsForType<ObjectMedium>("ObjectMedium", objectCount);
+	runAllTestsForType<ObjectBig>("ObjectBig", objectCount);
+	runAllTestsForType<ObjectMassive>("ObjectMassive", objectCount);
 
     return 0;
 }
